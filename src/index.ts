53356c864edb10209b5702d1e679046621177eaa
tselmuunzaya <tselmuunkoko@gmail.com>
2024-01-09 17:49:04 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+        res.send('Hello')
+})
+
+app.listen(port, () => {
+        console.log(`[server]: Server is runnig at http://localhost:${port}`)
+})
+
